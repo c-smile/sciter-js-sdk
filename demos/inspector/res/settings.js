13c6228c5c94ca6dc910548b2e6774f72de49567
8ctopus <hello@octopuslabs.io>
2022-03-03 14:33:32 +0500

    Save inspector window position and size

---
+++ demos/inspector/res/settings.js
@@ -0,0 +1,102 @@
+import * as env from "@env";
+import * as sys from "@sys";
+
+import {encode, decode} from "@sciter";
+
+let path;
+
+// list of persistable objects
+let list = [];
+
+// window position persistence
+add({
+  uiStateStore: function(data) {
+    const [x, y, w, h] = Window.this.box("xywh", "border", "screen");
+    data.window = {left: x, top: y, width: w, height: h};
+  },
+  uiStateRestore: function(data) {
+    if (data.window) {
+      const x = Math.max(data.window.left, 0);
+      const y = Math.max(data.window.top, 0);
+      const w = Math.max(data.window.width, 800);
+      const h = Math.max(data.window.height, 600);
+
+      // move to monitor
+      Window.this.move(x, y);
+
+      // replace on monitor
+      Window.this.move(x, y, w, h);
+    }
+  },
+});
+
+Window.this.on("move", saveState)
+  .on("size", saveState);
+
+export async function init(name) {
+  path = env.path("USER_APPDATA", name + ".json");
+  restore();
+}
+
+export function add(persistable) {
+  list.push(persistable);
+}
+
+export function saveState() {
+  if (!document.parentWindow) {
+    // document is unloaded
+    Window.this.off(saveState);
+    return;
+  }
+
+  // throttled request to store the data
+  document.timer(1000, store);
+}
+
+export async function store() {
+  if (!path)
+    return;
+
+  let file;
+
+  try {
+    file = await sys.fs.open(path, "w+", 0o666);
+
+    const data = {};
+    for (const persistable of list)
+      persistable.uiStateStore(data);
+
+    await file.write(encode(JSON.stringify(data, null, "  "), "utf-8"));
+
+    console.log("Settings stored");
+  }
+  catch (e) {
+    Window.this.modal(<warning>Cannot open file {path} for writing.<br/>{e}<br/>Settings will not be saved.</warning>);
+  }
+  finally {
+    if (file) file.close();
+  }
+}
+
+function restore() {
+  if (sys.fs.$stat(path) === null)
+    return false;
+
+  let buffer;
+
+  try {
+    buffer = sys.fs.$readfile(path, "r");
+  }
+  catch (e) {
+    return false;
+  }
+
+  try {
+    const data = JSON.parse(decode(buffer, "utf-8"));
+    for (const persistable of list)
+      persistable.uiStateRestore(data);
+  }
+  catch (e) {
+    console.error("Restore error:", e);
+  }
+}

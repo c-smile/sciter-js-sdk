a848990a255f083d583980db89a1fb3296bb8def
8ctopus <hello@octopuslabs.io>
2022-03-03 10:13:46 +0500

    Linted themes example js

--- samples.sciter/themes/android-material/theme.js
+++ samples.sciter/themes/android-material/theme.js
@@ -1,53 +1,65 @@
+export function Ripple() {
+  // ripple - expanding touch animation
+  let x;
+  let y;
+  let progress = 0;
+  const STEP = 0.03;
+  let step;
+  let radii = 100;
+  let h = 0;
+  let s = 1;
+  let l = 1;
+  let a = 1;
 
-export function Ripple() 
-{
-    // ripple - expanding touch animation     
-    var x,y;
-    var progress = 0.0; 
-    const STEP = 0.03;
-    var step; 
-    var radii = 100;
-    var h = 0, s = 1, l = 1, a = 1;
-      
-    const init = (evt) => {
-      let [width, height] = this.state.box("dimension"); 
-      radii = width > height ? width : height;
-      x = evt.x; y = evt.y;
-      progress = 0.0;
-      step = STEP;
-      let c = this.style.colorOf("background-color");
-      if( c ) [h,s,l,a] = c.hsl;
+  const init = (evt) => {
+    const [width, height] = this.state.box("dimension");
+    radii = width > height ? width : height;
+    x = evt.x;
+    y = evt.y;
+    progress = 0;
+    step = STEP;
+    const c = this.style.colorOf("background-color");
+    if (c) [h, s, l, a] = c.hsl;
+  };
+
+  const currentColor = () =>
+    Graphics.Color.hsl(h, s, l + (1 - l) * (1 - progress), a);
+
+  function paint(gfx) {
+    const color = currentColor();
+    gfx.pushLayer("background-area");
+    gfx.fillStyle = color;
+    gfx.beginPath();
+    gfx.arc(x, y, progress * radii, 0, Math.PI * 2, true);
+    gfx.closePath();
+    gfx.fill();
+    gfx.popLayer();
+  }
+
+  this.on("^mousedown", (evt) => {
+    init(evt);
+    const step = () => {
+      if (this.state.pressed) {
+        if (progress > 1) {
+          this.paintContent = null;
+          return;
+        }
+
+        progress += STEP;
+      } else {
+        if (progress <= 0) {
+          this.paintContent = null;
+          return;
+        }
+
+        progress -= STEP;
+      }
+
+      this.requestPaint();
+      requestAnimationFrame(step);
     };
 
-    const currentColor = () => Graphics.Color.hsl(h,s,l + (1.0 - l) * (1 - progress),a);
-
-    function paint(gfx) {
-      let color = currentColor();
-      gfx.pushLayer("background-area");
-      gfx.fillStyle = color;
-      gfx.beginPath();
-      gfx.arc(x, y, progress * radii, 0, Math.PI * 2, true);
-      gfx.closePath();
-      gfx.fill();
-      gfx.popLayer();
-    }
-
-    this.on("^mousedown", (evt) =>
-    {
-      init(evt);
-      const step = () => 
-      {
-        if(this.state.pressed) {
-          if(progress > 1.0) { this.paintContent = null; return; }   
-          progress += STEP;
-        } else {
-          if(progress <= 0.0) { this.paintContent = null; return; }
-          progress -= STEP;
-        }
-        this.requestPaint();
-        requestAnimationFrame(step);
-      };
-      this.paintContent = paint;
-      requestAnimationFrame(step);
-    });
+    this.paintContent = paint;
+    requestAnimationFrame(step);
+  });
 }
